14d419a30dd9f796d67b9fb0554d262087544c4e
Ali070 <Ali.zidan39@yahoo.com>
2025-09-19 15:52:49 +0300

    Finish sebha tab Finish onboarding screen

--- lib/ui/home/widgets/tabs/SebhaTab/SebhaTab.dart
+++ lib/ui/home/widgets/tabs/SebhaTab/SebhaTab.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SebhaTab extends StatelessWidget {
   const SebhaTab({super.key});
-
+  // create counter fo Azkar
   @override
   Widget build(BuildContext context) {
     return Container(color: Colors.orange,);
