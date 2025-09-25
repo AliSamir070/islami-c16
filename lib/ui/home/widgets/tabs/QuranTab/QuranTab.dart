f30af00ec44e3328c80c78aed80e9ba35f5e1df1
YoussefGerges27 <youssefgerges270@gmail.com>
2025-09-25 21:51:15 +0300

    + finished sebha screen

--- lib/ui/home/widgets/tabs/QuranTab/QuranTab.dart
+++ lib/ui/home/widgets/tabs/QuranTab/QuranTab.dart
@@ -18,7 +18,8 @@ class QuranTab extends StatelessWidget {
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage(AssetsManager.quranBack),fit: BoxFit.fill),
+        image: DecorationImage(
+            image: AssetImage(AssetsManager.quranBack), fit: BoxFit.fill),
       ),
       child: SafeArea(
         child: Padding(
@@ -28,92 +29,96 @@ class QuranTab extends StatelessWidget {
             children: [
               Align(
                   alignment: Alignment.center,
-                  child: Image.asset(AssetsManager.islamiLogo,width: width*0.75,fit: BoxFit.fitWidth,)),
-              SizedBox(height: 20,),
+                  child: Image.asset(
+                    AssetsManager.islamiLogo,
+                    width: width * 0.75,
+                    fit: BoxFit.fitWidth,
+                  )),
+              SizedBox(
+                height: 20,
+              ),
               TextField(
                 cursorColor: ColorManager.textfieldColor,
                 style: TextStyle(
-                  color: ColorManager.textfieldColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700
-                ),
+                    color: ColorManager.textfieldColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700),
                 decoration: InputDecoration(
-                  hintText: StringsManager.suraName,
-                  hintStyle: TextStyle(
-                      color: ColorManager.textfieldColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700
-                  ),
-                  prefixIconConstraints: BoxConstraints(
-                    maxHeight: 90,
-                    maxWidth: 90
-                  ),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(14),
-                    child: SvgPicture.asset(
-                      AssetsManager.quranIcon,
-                      height: 28,
-                      width: 28,
-                      colorFilter: ColorFilter.mode(
-                          ColorManager.goldColor,
-                          BlendMode.srcIn
+                    hintText: StringsManager.suraName,
+                    hintStyle: TextStyle(
+                        color: ColorManager.textfieldColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700),
+                    prefixIconConstraints:
+                        BoxConstraints(maxHeight: 90, maxWidth: 90),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(14),
+                      child: SvgPicture.asset(
+                        AssetsManager.quranIcon,
+                        height: 28,
+                        width: 28,
+                        colorFilter: ColorFilter.mode(
+                            ColorManager.goldColor, BlendMode.srcIn),
                       ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                          color:ColorManager.goldColor ,
-                          width: 1
-                      )
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                          color:ColorManager.goldColor ,
-                          width: 1
-                      )
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color:ColorManager.goldColor ,
-                      width: 1
-                    )
-                  )
-                ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: ColorManager.goldColor, width: 1)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: ColorManager.goldColor, width: 1)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: ColorManager.goldColor, width: 1))),
               ),
-              SizedBox(height: 20,),
-              Text(StringsManager.mostRecently,style: TextStyle(
-                color: ColorManager.textfieldColor,
-                fontSize: 16,
-                fontWeight: FontWeight.w700
-              ),),
-              SizedBox(height: 10,),
               SizedBox(
-                height: height*0.16,
+                height: 20,
+              ),
+              Text(
+                StringsManager.mostRecently,
+                style: TextStyle(
+                    color: ColorManager.textfieldColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: height * 0.16,
                 child: ListView.separated(
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) => RecentlyItem(),
-                    separatorBuilder: (context, index) => SizedBox(width: 10,),
+                    separatorBuilder: (context, index) => SizedBox(
+                          width: 10,
+                        ),
                     itemCount: 10),
               ),
-              SizedBox(height: 10,),
-              Text(StringsManager.surasList,style: TextStyle(
-                  color: ColorManager.textfieldColor,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700
-              ),),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                StringsManager.surasList,
+                style: TextStyle(
+                    color: ColorManager.textfieldColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 10,
+              ),
               Expanded(
                 child: ListView.separated(
-                    itemBuilder:(context, index) => SuraItem(AppConstants.surasList[index]) ,
+                    itemBuilder: (context, index) =>
+                        SuraItem(AppConstants.surasList[index]),
                     separatorBuilder: (context, index) => Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 44),
-                      child: Divider(),
-                    ),
-                    itemCount: AppConstants.surasList.length
-                ),
+                          padding: EdgeInsets.symmetric(horizontal: 44),
+                          child: Divider(),
+                        ),
+                    itemCount: AppConstants.surasList.length),
               )
             ],
           ),
